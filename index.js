281ae00e87314125ff2668fa2aed32d24e5811af
souleymane kone <desolo74@gmail.com>
2020-10-03 05:41:31 -0700

    change to index

--- index.js
+++ index.js
@@ -9,45 +9,37 @@ Use these methods: even.preventDefault(), togglesClass, .submit(), closes().
 
 $(function() {
 /* when input and submit $ add item  () even.preventDefault, submit*/
-    $('.js-shopping-list-form').on('submit', function(event){
+    $('#js-shopping-list-form').on('submit', function(event){
           event.preventDefault();
-           let userInput = $(event.currentTarget).find('#shopping-list-entry');
-               $("#shopping-list-entry").text(`${userInput.val()}`);
-                 userInput.val("");
+           const userInput = $("#shopping-list-entry").val();
+              userInput.val("");
+                 
       });  
- /* Added item text.val() is added to the list () .toggleClass, click*/
-    $("#shopping-item-toggle").on('click', '#shopping-item-toggle', function (event){
-           event.preventdefault();
-             $("#shopping-item").text($(event.shopping-item-toggle).text());
-                $( "#shopping-item").toggleClass(button-label, addOrRemove)
-
-    });
+    /* Added item text.val() is added to the list () .toggleClass, click*/
+$('.shopping-list').append(
+    `<li>
+      <span class="shopping-item">${listItem}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`);
+});
     /* deleted and item  */
     $("shopping-item").on('click', '#shopping-item-delete',function(event){
-        $(this).remove
+        $(this).closest('li').remove();
     });
-/* Line cross to the item () .closest */
-    $( "li.shopping-item__checked" ).closest( "li" ).css( "text-decoration", "line-through");
-    
-    
-
-/*  if not wanted Item removed 
-    $('ul').on('click', 'li', function(event) {
-        this.remove(); 
-      */      
+/* Line cross through the item () .closest */
+$('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
   });
 
-  /*  $(function() {
+       
   
-  $("button").click(function(event) {
-    $("ul").append(
-      "<li>" +
-      ["cat", "dog", "rock"][Math.floor(Math.random()*3)] + "</li>"
-    );
-  });
-  
-  $('li').click(function(event) {
-    this.remove();
-  });
-}); */
+
+
 
