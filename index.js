25150f066c88057855681296cbcf7302a69a29ae
souleymane kone <desolo74@gmail.com>
2020-10-03 18:56:45 -0700

    update indx.js

--- index.js
+++ index.js
@@ -4,42 +4,28 @@
 3rd uncheck
 4th remove
 
-Use these methods: even.preventDefault(), togglesClass, .submit(), closes().
+Use these methods: even.preventDefault(), togglesClass, .submit(), closest().
 */
 
 $(function() {
 /* when input and submit $ add item  () even.preventDefault, submit*/
     $('#js-shopping-list-form').on('submit', function(event){
-          event.preventDefault();
-           const userInput = $("#shopping-list-entry").val();
-              userInput.val("");
-                 
-      });  
-    /* Added item text.val() is added to the list () .toggleClass, click*/
-$('.shopping-list').append(
-    `<li>
-      <span class="shopping-item">${listItem}</span>
-      <div class="shopping-item-controls">
-        <button class="shopping-item-toggle">
-          <span class="button-label">check</span>
-        </button>
-        <button class="shopping-item-delete">
-          <span class="button-label">delete</span>
-        </button>
-      </div>
-    </li>`);
-});
-    /* deleted and item  */
-    $("shopping-item").on('click', '#shopping-item-delete',function(event){
-        $(this).closest('li').remove();
-    });
-/* Line cross through the item () .closest */
-$('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+        event.preventDefault();
+         let userInput = $(event.currentTarget).find('#shopping-list-entry');
+             $("#shopping-list-entry").text(`${userInput.val()}`);
+               userInput.val("");
+    });  
+/* Added find item apply .toggleClass, click*/
+  $(".shopping-item").on('click', '.shopping-item-toggle', function (event){
+         event.preventdefault();
+         $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+            
+
   });
-
-       
+  /* deleted and item  */
+  $("shopping-item").on('click', '#shopping-item-delete',function(event){
+      $(this).remove
+  });
+/* Line cross to the item () .closest */
+  $( "li.shopping-item__checked" ).closest( "li" ).css( "text-decoration", "line-through");
   
-
-
-
