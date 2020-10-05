a0638cc2f08e828c5acc6dd92d80d022dcf4fa94
souleymane kone <desolo74@gmail.com>
2020-10-05 04:46:13 -0700

    change again index.js

--- index.js
+++ index.js
@@ -9,39 +9,32 @@ Use these methods: even.preventDefault(), togglesClass, .submit(), closest().
 
 $(function() {
 /* when input and submit $ add item  () even.preventDefault, submit*/
-    $('#js-shopping-list-form').on('submit', function(event){
+     $('#js-shopping-list-form').submit(function(event){
         event.preventDefault();
-         let userInput = $(event.currentTarget).find('#shopping-list-entry');
-             $("#shopping-list-entry").text(`${userInput.val()}`);
-               userInput.val("");
-
+         const userInput = $("#shopping-list-entry").val();
+             $('#shopping-list-entry').val('');
 
    /* add li list of items with all values of botton and span*/            
-               $(".shopping-list").append(
-                 <li>
-                <span class="shopping-item">userInput</span>      
-                 <div class="shopping-item-controls">
-                   <button class="shopping-item-toggle">
-                     <span class="button-label">check</span>
-                   </button>
-                   <button class="shopping-item-delete">
-                     <span class="button-label">delete</span>
-                   </button>
-                 </div>
-               </li>);
+               $('.shopping-list').append(`<li>
+               <span class="shopping-item">${userInput}</span>      
+                <div class="shopping-item-controls">
+                  <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                  </button>
+                  <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                  </button>
+                </div>
+              </li>`);
     }); 
-    /* apply toggleClass */ 
-
-  $('.shopping-item-toggle').on('click', '.shopping-item', function (event){
-         event.preventDefault();
-      $(this).closest('li').find('.shopping-item').toggleClass('.shopping-item__checked');
-  });
   
   /* deleted and item  */
-  $(".shopping-item-delete").on('click', '.shopping-item',function(event){
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event){
       $(this).closest('li').remove();
-  });
-/* Line cross to the item () .closest */
-  $( "li.shopping-item__checked" ).closest( "li" ).css( "text-decoration", "line-through");
-  
+    });
+/* apply toggleClass */ 
+   $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+     $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+
 });
